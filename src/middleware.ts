e8b5eb41845bf28a5290d8c8a19496ad27d9c1af
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-08 10:55:34 -0300

    chore: setup next middleware with clerk auth helper

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const publicPaths = ['/', '/sign-in*']
+
+const isPublic = (path: string) => {
+	return publicPaths.find((x) =>
+		path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+	)
+}
+
+export default withClerkMiddleware((request: NextRequest) => {
+	if (isPublic(request.nextUrl.pathname)) {
+		return NextResponse.next()
+	}
+
+	const { userId } = getAuth(request)
+
+	if (!userId) {
+		const signInUrl = new URL('/sign-in', request.url)
+		signInUrl.searchParams.set('redirect_url', request.url)
+		return NextResponse.redirect(signInUrl)
+	}
+	return NextResponse.next()
+})
+
+export const config = {
+	matcher: ['/((?!static|.*\\..*|_next|favicon.ico).*)', '/'],
+}
