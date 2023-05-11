5a8264de9dca6c46c39d545d57c9fd3c0a776e2e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-10 22:55:09 -0300

    expose database url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { serverActions: true } }
+const nextConfig = {
+	experimental: { serverActions: true },
+	env: { DATABASE_URL: process.env.DATABASE_URL },
+}
 
 module.exports = nextConfig
