f838ed6986904436863867b9da1c8b11ab2fbe52
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-15 16:12:45 -0300

    chore: add new link on navbar

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,33 +1,45 @@
-import { Skeleton } from '@/components/ui/skeleton'
+import { ReactNode } from 'react'
 import { ClerkLoaded, ClerkLoading, RedirectToSignIn } from '@clerk/nextjs'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs/app-beta'
 import Link from 'next/link'
-import React, { ReactNode } from 'react'
 import { Mukta } from 'next/font/google'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const mukta = Mukta({ subsets: ['latin'], weight: '400' })
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
 		<>
 			<SignedIn>
-				<header className="p-5">
-					<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto text-md xl:text-xl">
-						<Link href="/home">Home</Link>
+				<header className="p-6 border-b-2 border-[#e7e5e4]">
+					<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto text-md xl:text-xl ">
+						<Link
+							href="/home"
+							className="duration-100 ease-in-out hover:text-emerald-400"
+						>
+							Home
+						</Link>
 
-						<ClerkLoading>
-							<Skeleton className="rounded-full w-9 h-9 " />
-						</ClerkLoading>
-						<ClerkLoaded>
-							<UserButton afterSignOutUrl="/" />
-						</ClerkLoaded>
-						<SignedOut>
-							<RedirectToSignIn />
-						</SignedOut>
+						<ul className="flex flex-row items-center space-x-4">
+							<li>
+								<Link href="/home/wish/create">Agregar nuevo deseo</Link>
+							</li>
+							<li>
+								<ClerkLoading>
+									<Skeleton className="bg-gray-300 rounded-full w-9 h-9 " />
+								</ClerkLoading>
+								<ClerkLoaded>
+									<UserButton afterSignOutUrl="/" />
+								</ClerkLoaded>
+								<SignedOut>
+									<RedirectToSignIn />
+								</SignedOut>
+							</li>
+						</ul>
 					</nav>
 				</header>
 			</SignedIn>
 
-			<main> {children}</main>
+			<main className={mukta.className}> {children}</main>
 		</>
 	)
 }
