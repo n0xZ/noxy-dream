0f316c34220b7c6d855bb4b6b100f951ca30530f
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-05-15 18:21:15 -0300

    chore: add new styles on wish iterm

--- src/components/wish/item.tsx
+++ src/components/wish/item.tsx
@@ -12,11 +12,13 @@ export function WishItem(props: Props) {
 				alt={`Avatar de ${props.wish.author?.name}`}
 				width={28}
 				height={28}
-				className="rounded-full"
+				className="rounded-full w-9 h-9"
 			/>
 			<aside className="flex flex-col w-full space-y-3">
-				<h2 className="font-medium text-center">{props.wish.author?.name}</h2>
-				<p className="text-gray-500">{props.wish.content}</p>
+				<h2 className="text-xl font-medium text-center">
+					{props.wish.author?.name} - {props.wish.title}
+				</h2>
+				<p className="text-sm text-center text-gray-500">{props.wish.content}</p>
 			</aside>
 		</article>
 	)
